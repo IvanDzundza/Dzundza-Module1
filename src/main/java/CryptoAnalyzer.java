4c4d2707ff0bffb86b11a860083f6c14a050b2c1
in <doker2008@gmail.com>
2024-11-11 17:16:57 +0200

    Add CryptoAnalyzer

---
+++ src/main/java/CryptoAnalyzer.java
@@ -0,0 +1,15 @@
+public class CryptoAnalyzer {
+
+    private final CaesarCipher caesarCipher = new CaesarCipher();
+
+    // Метод для атаки "грубої сили"
+    public String bruteForce(String encryptedText) {
+        StringBuilder result = new StringBuilder();
+        result.append("Possible decryptions:\n");
+        for (int shift = 1; shift < 26; shift++) {
+            String decryptedText = caesarCipher.decrypt(encryptedText, shift);
+            result.append("Shift ").append(shift).append(": ").append(decryptedText).append("\n");
+        }
+        return result.toString();
+    }
+}
