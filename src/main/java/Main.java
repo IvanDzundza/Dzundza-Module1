54e01b39b7e5f772ae335181a61a1b3d1bfcee21
in <doker2008@gmail.com>
2024-11-11 17:55:16 +0200

    Add Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,20 @@
 public class Main {
-    public static void main (String[] args) throws IOException {
-        FileService fileService = new FileService();
-        Chiper chiper = new Ceasar(Constants.ALPHABET_EN);
-        Runner runner = new Runner(chiper, fileService);
-        runner.run(args);
+    public static void main(String[] args) {
+        if (args.length < 2 || args.length > 3) {
+            System.out.println("Usage: java -jar myApp.jar <command> <filePath> [key]");
+            return;
+        }
+
+        String command = args[0];
+        String filePath = args[1];
+        String key = args.length == 3 ? args[2] : "";
+
+        Runner runner = new Runner();
+
+        try {
+            runner.run(command, filePath, key);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
